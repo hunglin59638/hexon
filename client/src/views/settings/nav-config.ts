9de5858db512aad151387dbd04e4114aab8caa6b
Hung-Lin, Chen <hunglin59638@gmail.com>
2022-09-23 18:44:41 +0800

    translate simple chinese to english

--- client/src/views/settings/nav-config.ts
+++ client/src/views/settings/nav-config.ts
@@ -12,7 +12,7 @@ export function useNavConfig() {
   const config = [
     {
       type: "item" as const,
-      text: "用户",
+      text: "User",
       icon: HIconName.Contact,
       color: vars.value.colorSuccess,
       key: "user" as const,
@@ -20,7 +20,7 @@ export function useNavConfig() {
     },
     {
       type: "item" as const,
-      text: "样式",
+      text: "Style",
       icon: HIconName.OEM,
       color: vars.value.colorWarning,
       key: "style" as const,
@@ -28,7 +28,7 @@ export function useNavConfig() {
     },
     {
       type: "item" as const,
-      text: "关于",
+      text: "About",
       icon: HIconName.Info,
       color: vars.value.textColorSecondary,
       key: "about" as const,
